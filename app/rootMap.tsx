01282185faaa27b1579f25ec1b2e7ad91ccaea9c
ojela028xamk <jerelans91@gmail.com>
2025-01-04 13:12:43 +0200

    map component

---
+++ app/rootMap.tsx
@@ -0,0 +1,11 @@
+import css from "./rootMap.module.scss";
+
+const RootMap = () => {
+  return (
+    <div className={css.root_map}>
+      <h1>Suomi</h1>
+    </div>
+  );
+};
+
+export default RootMap;
