d0ffaab1a698c4990843df7c894579065bfd60ae
ojela028xamk <jerelans91@gmail.com>
2025-01-05 13:08:55 +0200

    components for two games

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
+import CityQuiz from "./cityQuiz/page";
 import css from "./page.module.scss";
-import RootMap from "./rootMap";
+import ProvinceQuiz from "./provinceQuiz/page";
 
 export default function Home() {
   return (
     <div className={css.page}>
-      <RootMap />
+      <CityQuiz />
+      <ProvinceQuiz />
     </div>
   );
 }
