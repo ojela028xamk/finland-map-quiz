82529b8da265501298075f23f75a82824c609052
ojela028xamk <jerelans91@gmail.com>
2025-01-10 13:20:41 +0200

    province list for the game

--- app/globalTypes.ts
+++ app/globalTypes.ts
@@ -9,3 +9,25 @@ export type Game = {
   description: string;
   link: string;
 };
+
+export enum Province {
+  LAPLAND = "Lapland",
+  NORTHERN_OSTROBOTHNIA = "Northern Ostrobothnia",
+  CENTRAL_OSTROBOTHNIA = "Central Ostrobothnia",
+  SOUTHERN_OSTROBOTHNIA = "Southern Ostrobothnia",
+  OSTROBOTHNIA = "Ostrobothnia",
+  KAINUU = "Kainuu",
+  NORTH_KARELIA = "North Karelia",
+  SOUTH_KARELIA = "South Karelia",
+  NORTHERN_SAVONIA = "Northern Savonia",
+  SOUTHERN_SAVONIA = "Southern Savonia",
+  CENTRAL_FINLAND = "Central Finland",
+  SATAKUNTA = "Satakunta",
+  PIRKANMAA = "Pirkanmaa",
+  PÄIJÄNNE_TAVASTIA = "Päijänne Tavastia",
+  TAVASTIA_PROPER = "Tavastia Proper",
+  SOUTHWEST_FINLAND = "Southwest Finland",
+  UUSIMAA = "Uusimaa",
+  KYMENLAAKSO = "Kymenlaakso",
+  ÅLAND_ISLANDS = "Åland Islands",
+}
--- app/provinceQuiz/page.tsx
+++ app/provinceQuiz/page.tsx
@@ -1,10 +1,11 @@
 "use client";
-import { useLayoutEffect } from "react";
+import { useLayoutEffect, useState } from "react";
 import * as am5 from "@amcharts/amcharts5";
 import * as am5map from "@amcharts/amcharts5/map";
 import am5geodata_finlandLow from "@amcharts/amcharts5-geodata/finlandLow";
 import css from "./provinceQuiz.module.scss";
 import { translateProvinceName } from "../services/helperService";
+import { Province } from "../globalTypes";
 
 type DataContextProvince = {
   CNTRY: string;
@@ -21,7 +22,30 @@ type Geometry = {
   type: string;
 };
 
+type ProvinceGameItem = {
+  name: string;
+  isAnswered: boolean;
+  isCorrect: boolean | null;
+};
+
+const initialList: ProvinceGameItem[] = Object.keys(Province).map(
+  (province) => {
+    const provinceItem: ProvinceGameItem = {
+      name: province,
+      isAnswered: false,
+      isCorrect: null,
+    };
+
+    return provinceItem;
+  }
+);
+
 const ProvinceQuiz = () => {
+  const [provinceGameList, setProvinceGameList] =
+    useState<ProvinceGameItem[]>(initialList);
+
+  console.log(provinceGameList);
+
   useLayoutEffect(() => {
     let root = am5.Root.new("chartdiv");
 
