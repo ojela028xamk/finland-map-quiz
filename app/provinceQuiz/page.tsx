eb5d33ad752e42a4d5766c32d1bd0959d7275665
ojela028xamk <jerelans91@gmail.com>
2025-01-18 12:12:57 +0200

    guiz on flexbox

--- app/provinceQuiz/page.tsx
+++ app/provinceQuiz/page.tsx
@@ -169,7 +169,6 @@ const ProvinceQuiz = () => {
 
   return (
     <div className={css.province_quiz}>
-      <h1>Province Quiz</h1>
       <div className={css.province_quiz_display}>
         Choose: {translateProvinceName(currentProvince)}
         <br />
