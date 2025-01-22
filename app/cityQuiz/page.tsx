22d03618f00dfee02ba60994cd49522378279474
ojela028xamk <jerelans91@gmail.com>
2025-01-22 14:37:26 +0200

    console log city name on click

--- app/cityQuiz/page.tsx
+++ app/cityQuiz/page.tsx
@@ -6,6 +6,19 @@ import am5geodata_finlandLow from "@amcharts/amcharts5-geodata/finlandLow";
 import css from "./cityQuiz.module.scss";
 import { cities } from "./cityQuizData";
 
+type DataContextCity = {
+  geometry: Geometry;
+  geometryType: string;
+  id: undefined;
+  madeFromGeoData: boolean;
+  name: string;
+};
+
+type Geometry = {
+  coordinates: number[];
+  type: string;
+};
+
 const CityQuiz = () => {
   useLayoutEffect(() => {
     let root = am5.Root.new("chartdiv");
@@ -31,14 +44,26 @@ const CityQuiz = () => {
       })
     );
 
-    pointSeries.bullets.push(function () {
+    pointSeries.bullets.push(function (root) {
+      const circle = am5.Circle.new(root, {
+        radius: 10,
+        fill: am5.color(0x6771dc),
+        stroke: am5.color("#000000"),
+        strokeWidth: 1,
+      });
+
+      circle.events.on("click", function (event) {
+        const dataItem = event.target.dataItem;
+
+        if (dataItem) {
+          const dataContext = dataItem.dataContext as DataContextCity;
+          const clickedCity = dataContext.name;
+          console.log(clickedCity);
+        }
+      });
+
       return am5.Bullet.new(root, {
-        sprite: am5.Circle.new(root, {
-          radius: 10,
-          fill: am5.color(0x6771dc),
-          stroke: am5.color("#000000"),
-          strokeWidth: 2,
-        }),
+        sprite: circle,
       });
     });
 
