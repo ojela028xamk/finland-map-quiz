d52dc2d79c2240d3d634990bb3a151d0d0752bcc
ojela028xamk <jerelans91@gmail.com>
2025-01-22 13:30:57 +0200

    city data to different file

---
+++ app/cityQuiz/cityQuizData.ts
@@ -0,0 +1,205 @@
+export const cities: GeoJSON.GeoJSON = {
+  type: "FeatureCollection",
+  features: [
+    {
+      type: "Feature",
+      properties: {
+        name: "Helsinki",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [24.966977427417085, 60.168322420225955],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "Espoo",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [24.659990087976592, 60.20529617119421],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "Tampere",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [23.764598764385628, 61.49684641686608],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "Vantaa",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [25.044459196484684, 60.29365923855584],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "Oulu",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [25.48717881847835, 65.0088109508569],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "Turku",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [22.249040742469788, 60.45065334348977],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "Jyväskylä",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [25.73271420302743, 62.23490233090804],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "Kuopio",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [27.666059467048417, 62.89376227293934],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "Lahti",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [25.651019540283695, 60.98407068030647],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "Pori",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [21.80699540175393, 61.48507409952892],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "Joensuu",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [29.7958200739215, 62.598197975936074],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "Kouvola",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [26.764462602109802, 60.87492357473989],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "Lappeenranta",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [28.203502158113, 61.05640988461391],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "Vaasa",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [21.617836098616323, 63.09520206094629],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "Hämeenlinna",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [24.453205087574066, 60.99027841520249],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "Seinäjoki",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [22.852885015335303, 62.78802587515736],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "Rovaniemi",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [25.859466871159984, 66.50136128313092],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "Mikkeli",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [27.333378113145848, 61.68564706328189],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "Porvoo",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [25.649985566853044, 60.39332212773805],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "Salo",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [23.117664878041737, 60.38972009787064],
+      },
+    },
+  ],
+};
